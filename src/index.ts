08b960a929a9e8e72555bdf2bb53449cef0eb8ad
viktor <viktor@neverbland.com>
2019-08-11 22:49:07 +0100

    Adds examples v1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,65 @@
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = "Hello world";
-
-  return element;
+interface ProductData {
+  id: string;
+  price: number;
 }
 
-document.body.appendChild(component());
+interface MappedProductData {
+  id: string;
+  price: number;
+  priceWithVat: number;
+}
+
+// Problem: business logic mixed with query!
+// The logic could be reusable
+function Example1() {
+  const productData: ProductData = queryProduct();
+
+  const mappedProductData: MappedProductData = {
+    ...productData,
+    priceWithVat: productData.price * 1.2
+  };
+
+  return mappedProductData;
+}
+
+// Better, we can now reuse mapProductData across the site, 
+// so that the BL stays the same
+function Example2() {
+  const productData: ProductData = queryProduct();
+  const mappedProductData = mapProductData(productData);
+  return mappedProductData;
+
+  function mapProductData(productData: ProductData) {
+    return {
+      ...productData,
+      priceWithVat: productData.price * 1.2
+    };
+  }
+}
+
+// Generic example, where we can truly reuse the business logic across a full range of objects
+function Example3() {
+  const productData: ProductData = queryProduct();
+  const mappedProductData = mapProductData(productData);
+  return mappedProductData;
+
+  function mapProductData<T extends { price: number }>(productData: T) {
+    return {
+      ...productData,
+      priceWithVat: productData.price * 1.2
+    };
+  }
+}
+
+console.log(Example1());
+console.log(Example2());
+console.log(Example3());
+
+function queryProduct() {
+  const product: ProductData = {
+    price: 12,
+    id: "foo"
+  };
+
+  return product;
+}
